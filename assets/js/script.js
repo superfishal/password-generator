c4843bcccdf01a3500e4f7e04b0f5670096ac85f
superfishal <97133716+superfishal@users.noreply.github.com>
2022-02-18 16:20:21 -0800

    conditional statements for generatedValues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,27 @@
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numberic = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!","#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-}
-
+};
 // variable for password length
 var passLength = function() {
   var lengthPrompt = window.prompt("How long would you like your password? Min 8 characters, Max 128 characters.");
   if(lengthPrompt < 8 || lengthPrompt > 128) {
     window.prompt("Please input a number between 8-128.");
-    return passLength();
+    return lengthPrompt;
   }
-  return lengthPrompt;
+  return passLength();
+  console.log(passLength);
 }
 
 var lowerCase = function(){
@@ -30,15 +35,32 @@ var upperCase = function(){
 };
 
 var numeric = function(){
-  var numericCaseConf = window.confirm("Include numbers? Press OK for yes, and CANCEL for no");
-  return numericCaseConf;
+  var numericConf = window.confirm("Include numbers? Press OK for yes, and CANCEL for no");
+  return numericConf;
 };
 
+var special = function(){
+  var specialConf = window.confirm("Include special characters? Press OK for yes, and CANCEL for no");
+  return specialConf;
+};
+// var generatePassword = function(){
+//   var generatedNumeric = numeric();
+//   var generatedUpperCase = upperCase();
+//   var generatedLowerCase = lowerCase();
+//   var generatedSpecial = special();
+// }
+var generatePassword = function(){
 
-
-var generatePassword 
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+for (var i = 0; i < generatedNumeric; i++){
+  var generatedNumeric = Math.floor(Math.random() * numeric.length);
+};
+for (var i = 0; i < generatedUpperCase; i++){
+  var generatedUpperCase = Math.floor(Math.random() * upperCase.length);
+};
+for (var i = 0; i < generatedLowerCase; i++){
+  var generatedLowerCase = Math.floor(Math.random() * lowerCase.length);
+};
+for (var i = 0; i < generatedSpecial; i++){
+  var generatedSpecial = Math.floor(Math.random() * special.length);
+};
+};
