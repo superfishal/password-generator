b2d4e5099f31d4cb4b1b23b6ff9c28c196cc8b74
superfishal <97133716+superfishal@users.noreply.github.com>
2022-02-19 15:55:35 -0800

    need to separate functions out of generate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,128 @@
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numberic = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var special = ["!","#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
+upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+specialArray = ["!","#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "<", "=", ">", "?", "@", "^", "_", "`", "~"];
+possibleChar = [];
+// need a results array? to take whats generated in random password
+// function randomWhatever () {
+// return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+//  }
+// Character Code for JavaScript
 
-var possibleChar = [];
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// need to separate confirm function , randomize function, gather randomize function, display randomize function
+var generatePassword = function(){
 
-// variable for password length
+  // variable for password length
 var passLength = function() {
   var lengthPrompt = window.prompt("How long would you like your password? Min 8 characters, Max 128 characters.");
+  lengthPrompt = parseInt(lengthPrompt);
   if(lengthPrompt < 8 || lengthPrompt > 128) {
-
-    return lengthPrompt;
+    window.alert("Choose a number between 8-128");
+    possibleChar.push(passLength);
+    console.log(passLength());
   };
-  return passLength();
 };
-// console.log(passLength()); // HOW CAN WE TEST THIS??
 
-// lower case prompt to confirm -- WHY ARE THEY STRINGS?
+
+
+
+// lower case prompt to confirm
 var lowerCase = function(){
   var lowerCaseConf = window.confirm("Include lowercase characters? Press OK for yes, and CANCEL for no.");
   if(lowerCaseConf === true) {
-  possibleChar.push(lowerCase);
-}
+  var lowerCaseLength = lowerCaseArray.length +1;
+  result.push(lowerCase[Math.floor(Math.random() * lowerCaseLength)]);
+  possibleChar.push(lowerCaseLength);
+  };
 };
+
+
+
 //upper case prompt to confirm
 var upperCase = function(){
   var upperCaseConf = window.confirm("Include UPPERCASE characters? Press OK for yes, and CANCEL for no.");
   if(upperCaseConf === true) {
-  possibleChar.push(upperCase);
+  var upperCaseLength = upperCaseArray.length +1;
+  result.push(upperCase[Math.floor(Math.random() * upperCaseLength)]);
+  possibleChar.push(upperCaseLength);
   };
 };
+
+
+
 // numeric characters prompt to confirm
 var numeric = function(){
   var numericConf = window.confirm("Include numbers? Press OK for yes, and CANCEL for no");
   if(numericConf === true) {
-    possibleChar.push(numeric);
+    var numericLength = numericArray.length +1;
+    result.push(numeric[Math.floor(Math.random() * numericLength)]);
+    possibleChar.push(numericLength);
   };
 };
+
+
+
 // special character prompt to confirm
 var special = function(){
   var specialConf = window.confirm("Include special characters? Press OK for yes, and CANCEL for no");
   if(specialConf === true) {
-    possibleChar.push(special);
+    var specialLength = specialArray.length +1;
+    result.push(special[Math.floor(Math.random() * specialLength)]);
+    possibleChar.push(specialLength);
   };
 };
-// turned these into for statements inside generatePassword function to choose a random part of each string
-var generatePassword = function(){
+};
 
-for (var i = 0; i < generatedNumeric; i++){
-  var generatedNumeric = Math.floor(Math.random() * numeric.length); // why is this a function
-};
-for (var i = 0; i < generatedUpperCase; i++){
-  var generatedUpperCase = Math.floor(Math.random() * upperCase.length);
-};
-for (var i = 0; i < generatedLowerCase; i++){
-  var generatedLowerCase = Math.floor(Math.random() * lowerCase.length);
-};
-for (var i = 0; i < generatedSpecial; i++){
-  var generatedSpecial = Math.floor(Math.random() * special.length);
-};
-};
-console.log(generatePassword());
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  // password.toString(); or password = password.join();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+  console.log(password);
 };
 
-// writePassword();
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+// get.addEventListener("click", function () {
+//   ps = generatePassword();
+//   document.getElementById("password").placeholder = ps;
+// });
+
+
+// Choices declared outside the if statement so they can be concatenated upon condition
+// var choices;
+  // Start random selection variables:
+  // Random selection for all variables: 
+  //   for (var i = 0; i < enter; i++) {
+  //     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
+  //     password.push(pickChoices);
+  // }
+// This joins the password array and converts it to a string
+//     var ps = password.join("");
+//     UserInput(ps);
+//     return ps;
+// }
+
+
+
+
+
+// for (var i = 0; i < generatedNumeric; i++){
+//   var generatedNumeric = Math.floor(Math.random() * numeric.length); // why is this a function
+// };
+// for (var i = 0; i < generatedUpperCase; i++){
+//   var generatedUpperCase = Math.floor(Math.random() * upperCase.length);
+// };
+
+// for (var i = 0; i < generatedSpecial; i++){
+//   var generatedSpecial = Math.floor(Math.random() * special.length);
+// };
+// for .....
+//  numeric
+
